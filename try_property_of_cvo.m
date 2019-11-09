dfe5c1d9a1ad3e5e468b2ce1bf92c39fee405549
Minghan Zhu <minghanz@umich.edu>
2019-11-09 11:33:14 -0500

    change quotation marks to be compatible in win

--- try_property_of_cvo.m
+++ try_property_of_cvo.m
@@ -2,7 +2,7 @@ clearvars
 %% create 2D point cloud
 [xy1, xy2_, f1_, f2_] = generate_pcd(3); % options 1,2,3
 %% calculate the function corresponding to a point cloud
-to_min = "dist"; %"dist" or "inner_prod"
+to_min = 'dist'; %'dist' or 'inner_prod'
 l = 0.3;
 n_change = 11;
 step = 0.1;
@@ -25,12 +25,12 @@ for i = 1:n_change
     plot(xy1(1,:), xy1(2,:));%,'bx');
     hold on;
     plot(xy2(1,:), xy2(2,:));%, 'ro');
-    if to_min == "dist"
+    if to_min == 'dist'
         arrow(xy2', (xy2 + grad2_dist*0.2)'); % arrow([x1, y1],[x2,y2])
-    elseif to_min == "inner_prod"
+    elseif to_min == 'inner_prod'
         arrow(xy2', (xy2 + grad2*0.2)');
     else
-        disp("Not recognized");
+        disp('Not recognized');
     end
 %     arrow([0,0], grad_pose12'/norm(grad_pose12), 'Color' ,'r');
     arrow([0,0], grad_pose12', 'Color' ,'r');
@@ -40,8 +40,8 @@ end
 %% 
 func_dist = prods1 + prods2 - 2*prods;
 figure(2);
-if to_min == "dist"
+if to_min == 'dist'
     plot(changes, func_dist);
-elseif to_min == "inner_prod"
+elseif to_min == 'inner_prod'
     plot(changes, prods);
 end
